9d1a5deff3346bec7ce5e968ae1ce47001d8a0d7
Rémi Jean <remijean25@gmail.com>
2021-07-30 17:47:50 +0200

    0.8

--- player/hud.gd
+++ player/hud.gd
@@ -1,6 +1,1 @@
 extends Control
-
-onready var ammo := $Ammo
-
-func _on_Gun_ammo_changed(new_ammo):
-	ammo.set_text(String(new_ammo))
--- weapons/gun/gun.gd
+++ weapons/gun/gun.gd
@@ -1,12 +1,6 @@
 extends Control
 
-signal ammo_changed(new_ammo)
-
-var current_ammo := MAX_AMMO
-var isReloading = false
 var isShooting = false
-
-const MAX_AMMO = 10
 
 onready var animation_player := $AnimationPlayer
 
@@ -16,32 +10,13 @@ func _on_Player_shoot(weapon, _delta):
 		pass
 	
 	# Shoot
-	if !isShooting && !isReloading:
+	if !isShooting:
 		isShooting = true
-		var new_ammo = current_ammo
-		if current_ammo:
-			animation_player.play("shoot")
-			new_ammo -= 1
-		else:
-			animation_player.play("dry")
+		animation_player.play("shoot")
 		yield(animation_player, "animation_finished")
-		set_ammo(new_ammo)
 		isShooting = false
 
 func _on_Player_reload(weapon, _delta):
 	# Do nothing if the weapon is not used
 	if weapon != name:
 		pass
-	
-	# Reload
-	if !isReloading:
-		isShooting = false
-		isReloading = true
-		animation_player.play("reload")
-		yield(animation_player, "animation_finished")
-		set_ammo(MAX_AMMO)
-		isReloading = false
-
-func set_ammo(new_ammo):
-	current_ammo = new_ammo
-	emit_signal("ammo_changed", new_ammo)
